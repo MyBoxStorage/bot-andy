fa10b9e57adfbbbe2c2bd1d6daf9fd892a95a139
ChapolinSheik <techgeniushq@gmail.com>
2026-05-25 04:58:07 -0300

    fix: logo in public, new tunnel url

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-/** Atualizado em 2026-05-25 02:56 por scripts/cenario-b.ps1 */
-window.__API_BASE__ = 'https://istanbul-married-dispatched-rotary.trycloudflare.com'
-window.__VERCEL_ORIGIN__ = 'https://bot-andy.vercel.app'
+﻿/** Atualizado em 2026-05-25 04:57 por scripts/cenario-b.ps1 */
+window.__API_BASE__ = 'https://packs-variable-apparel-sol.trycloudflare.com'
+window.__VERCEL_ORIGIN__ = 'https://andy-na-regua.vercel.app'
