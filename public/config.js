594ba5c27e30f2bf4e08280e48787f143ece7aea
ChapolinSheik <techgeniushq@gmail.com>
2026-05-25 05:19:17 -0300

    fix: ngrok url + correct vercel origin

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
 ﻿/** Atualizado em 2026-05-25 04:57 por scripts/cenario-b.ps1 */
-window.__API_BASE__ = 'https://packs-variable-apparel-sol.trycloudflare.com'
-window.__VERCEL_ORIGIN__ = 'https://andy-na-regua.vercel.app'
+window.__API_BASE__ = 'https://connivingly-unsulfureous-miesha.ngrok-free.dev'
+window.__VERCEL_ORIGIN__ = 'https://bot-andy.vercel.app'
